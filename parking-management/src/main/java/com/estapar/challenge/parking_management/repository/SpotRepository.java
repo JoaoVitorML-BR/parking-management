b68064e8d0bd5533b8f8345d6d654098729ec75c
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-06-25 20:47:19 -0300

    feat: adds JPA repositories for managing parking spots and garages

--- parking-management/src/main/java/com/estapar/challenge/parking_management/repository/SpotRepository.java
+++ parking-management/src/main/java/com/estapar/challenge/parking_management/repository/SpotRepository.java
@@ -1,9 +1,13 @@
 package com.estapar.challenge.parking_management.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.estapar.challenge.parking_management.models.Spot;
 
 public interface SpotRepository extends JpaRepository<Spot, Integer> {
-    
+    List<Spot> findBySectorCode(String sectorCode);
+    Optional<Spot> findById(Integer id);
 }
