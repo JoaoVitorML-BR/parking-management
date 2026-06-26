acddab3800a01b3d89b16d199875bdbeb63f1fb1
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-06-26 09:59:29 -0300

    feat: implements global exceptions

---
+++ parking-management/src/main/java/com/estapar/challenge/parking_management/exception/GlobalExceptionHandler.java
@@ -0,0 +1,69 @@
+package com.estapar.challenge.parking_management.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NoAvailableSpotException.class)
+    public ResponseEntity<Map<String, Object>> handleNoAvailableSpotException(NoAvailableSpotException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "error");
+        response.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
+
+    @ExceptionHandler(SectorNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleSectorNotFoundException(SectorNotFoundException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "error");
+        response.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(VehicleAlreadyParkedException.class)
+    public ResponseEntity<Map<String, Object>> handleVehicleAlreadyParkedException(VehicleAlreadyParkedException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "error");
+        response.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
+
+    @ExceptionHandler(ParkingLotFullException.class)
+    public ResponseEntity<Map<String, Object>> handleParkingLotFullException(ParkingLotFullException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "error");
+        response.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
+    }
+
+    @ExceptionHandler(FindAvaliableSectorException.class)
+    public ResponseEntity<Map<String, Object>> handleFindAvaliableSectorException(FindAvaliableSectorException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "error");
+        response.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(CalculateDynamicPriceException.class)
+    public ResponseEntity<Map<String, Object>> handleCalculateDynamicPriceException(CalculateDynamicPriceException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "error");
+        response.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+}
