ac49591afa617d9190f7b9f95b51053aa766c3d8
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-06-25 21:18:31 -0300

    feat: implements finds tickets funcions

--- parking-management/src/main/java/com/estapar/challenge/parking_management/repository/TicketRepository.java
+++ parking-management/src/main/java/com/estapar/challenge/parking_management/repository/TicketRepository.java
@@ -1,5 +1,8 @@
 package com.estapar.challenge.parking_management.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.estapar.challenge.parking_management.models.Ticket;
 
 public interface TicketRepository extends JpaRepository<Ticket, UUID> {
+    Optional<Ticket> findByLicensePlateAndExitTimeIsNull(String licensePlate);
 
+    List<Ticket> findBySectorCodeAndExitTimeBetween(String sectorCode, LocalDateTime start, LocalDateTime end);
 }
