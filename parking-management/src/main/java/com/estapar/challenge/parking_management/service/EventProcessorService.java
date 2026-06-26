11f4abf76c872a8e3c80ba127dd3563b92e0c90a
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-06-26 20:33:52 -0300

    feat: Implemented functionality to process entry, parked, and exit data.

--- parking-management/src/main/java/com/estapar/challenge/parking_management/service/EventProcessorService.java
+++ parking-management/src/main/java/com/estapar/challenge/parking_management/service/EventProcessorService.java
@@ -3,138 +3,56 @@ package com.estapar.challenge.parking_management.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import com.estapar.challenge.parking_management.dto.WebhookEventDTO;
-import com.estapar.challenge.parking_management.exception.NoAvailableSpotException;
-import com.estapar.challenge.parking_management.exception.SectorNotFoundException;
-import com.estapar.challenge.parking_management.exception.VehicleAlreadyParkedException;
-import com.estapar.challenge.parking_management.models.Garage;
-import com.estapar.challenge.parking_management.models.Spot;
-import com.estapar.challenge.parking_management.models.Ticket;
-import com.estapar.challenge.parking_management.repository.GarageRepository;
-import com.estapar.challenge.parking_management.repository.SpotRepository;
-import com.estapar.challenge.parking_management.repository.TicketRepository;
-
-import jakarta.transaction.Transactional;
+import com.estapar.challenge.parking_management.usecase.entry.EntryUseCase;
+import com.estapar.challenge.parking_management.usecase.exit.ExitUseCase;
+import com.estapar.challenge.parking_management.usecase.parked.ParkedUseCase;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class EventProcessorService {
 
-    private final SpotRepository spotRepository;
-    private final TicketRepository ticketRepository;
-    private final GarageRepository garageRepository;
+    private final EntryUseCase entryUseCase;
+    private final ParkedUseCase parkedUseCase;
+    private final ExitUseCase exitUseCase;
 
-    @Transactional
     public void process(WebhookEventDTO event) {
+
         String eventType = event.getEventType();
         String plate = event.getLicensePlate();
 
         log.info("Processing {} event for license plate {}", eventType, plate);
 
-        if ("ENTRY".equals(eventType)) {
-            processEntry(event);
-        } else {
-            log.info("Event {} still not implemented, only logging", eventType);
+        String normalizedEventType = normalizeEventType(eventType);
+
+        log.info("Original event: '{}'", eventType);
+        log.info("Normalized event: '{}'", normalizedEventType);
+
+        switch (normalizedEventType) {
+
+            case "ENTRY" -> entryUseCase.execute(event);
+
+            case "PARKED" -> parkedUseCase.execute(event);
+
+            case "EXIT" -> exitUseCase.execute(event);
+
+            default -> log.warn("Unknown event {}", eventType);
         }
     }
 
-    private void processEntry(WebhookEventDTO event) {
-        String plate = event.getLicensePlate();
-        if (ticketRepository.findByLicensePlateAndExitTimeIsNull(plate).isPresent()) {
-            throw new VehicleAlreadyParkedException(plate);
+    public String normalizeEventType(String event) {
+        if (event == null) {
+            return "UNKNOWN";
         }
 
-        Spot availableSpot = findAvailableSpot();
-
-        String sector = availableSpot.getSectorCode();
-
-        BigDecimal dynamicFactor = calculateDynamicPriceFactor(sector);
-
-        log.info("Dynamic pricing factor: {}", dynamicFactor);
-
-        availableSpot.setIsOccupied(true);
-
-        Ticket ticket = new Ticket();
-        ticket.setSpotId(availableSpot.getId());
-        ticket.setLicensePlate(plate);
-        ticket.setSectorCode(sector);
-        ticket.setEntryTime(event.getEntryTime());
-        ticket.setDynamicPriceFactor(dynamicFactor);
-
-        spotRepository.save(availableSpot);
-        ticketRepository.save(ticket);
-
-        log.info("Ticket {} created for vehicle {}", ticket.getId(), plate);
-        log.info("ENTRY processed successfully for {}", plate);
-    }
-
-    private Spot findAvailableSpot() {
-
-        List<Garage> garages = garageRepository.findAll();
-
-        for (Garage garage : garages) {
-
-            String sector = garage.getSectorCode();
-
-            long occupied = spotRepository.countBySectorCodeAndIsOccupiedTrue(sector);
-
-            if (occupied >= garage.getMaxCapacity()) {
-                continue;
-            }
-
-            var spot = spotRepository
-                    .findFirstBySectorCodeAndIsOccupiedFalse(sector);
-
-            if (spot.isPresent()) {
-                log.info(
-                        "Spot {} selected in sector {}",
-                        spot.get().getId(),
-                        sector);
-
-                return spot.get();
-            }
+        String normalized = event.trim().toUpperCase().split(" ")[0];
+        if (normalized.matches("ENTRY|PARKED|EXIT")) {
+            return normalized;
         }
 
-        throw new NoAvailableSpotException("Parking lot is full.");
-    }
-
-    private BigDecimal calculateDynamicPriceFactor(String sector) {
-
-        long maxCapacity = garageRepository.findBySectorCode(sector)
-                .orElseThrow(() -> new SectorNotFoundException(sector))
-                .getMaxCapacity();
-
-        long occupiedSpots = spotRepository.countBySectorCodeAndIsOccupiedTrue(sector);
-
-        if (maxCapacity == 0) {
-            return BigDecimal.ONE;
-        }
-
-        double occupancyRate = (double) occupiedSpots / maxCapacity;
-
-        log.info(
-                "Sector {} occupancy: {}%",
-                sector,
-                String.format("%.2f", occupancyRate * 100));
-
-        if (occupancyRate < 0.25) {
-            return new BigDecimal("0.90");
-        }
-
-        if (occupancyRate < 0.50) {
-            return BigDecimal.ONE;
-        }
-
-        if (occupancyRate < 0.75) {
-            return new BigDecimal("1.10");
-        }
-
-        return new BigDecimal("1.25");
+        return "UNKNOWN";
     }
 }
