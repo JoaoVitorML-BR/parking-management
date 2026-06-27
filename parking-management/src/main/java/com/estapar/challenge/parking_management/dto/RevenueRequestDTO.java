5163824636a24b193d6c502b43ae7b5da271a540
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-06-27 20:39:57 -0300

    feat: Added data description for Swagger.

--- parking-management/src/main/java/com/estapar/challenge/parking_management/dto/RevenueRequestDTO.java
+++ parking-management/src/main/java/com/estapar/challenge/parking_management/dto/RevenueRequestDTO.java
@@ -1,11 +1,15 @@
 package com.estapar.challenge.parking_management.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(description = "Requisição de consulta de faturamento")
 public class RevenueRequestDTO {
+    @Schema(description = "Data de consulta", example = "2026-06-26")
     private String date;
+    @Schema(description = "Setor de consulta", example = "A")
     private String sector;
 }
